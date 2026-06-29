164fe4e95df87de73c7a7ee4433a4ecd79e3f6e7
Aria Persian <160272091+ariaPersian@users.noreply.github.com>
2026-06-30 01:42:48 +0330

    Mock preferences in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:ergomove/main.dart';
 
 void main() {
+  setUp(() {
+    SharedPreferences.setMockInitialValues(<String, Object>{});
+  });
+
   testWidgets('ErgoMove loads the bilingual timer UI', (tester) async {
     await tester.pumpWidget(const ErgoMoveApp());
     await tester.pumpAndSettle();
